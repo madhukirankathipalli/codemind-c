5ca99ef50c61674d713463344079bc81ec6463bb
madhukirankathipalli <119720465+madhukirankathipalli@users.noreply.github.com>
2022-12-22 14:48:25 +0530

    #include<stdio.h> int main() {     int tc,k;     scanf("%d",&tc);     for(k=1;k<=tc;k++)     {         int a,b;         scanf("%d%d",&a,&b);         printf("%d ",a+b);
        }
        
    }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int tc,k; 
+    scanf("%d",&tc);
+    for(k=1;k<=tc;k++) 
+    {
+        int a,b;
+        scanf("%d%d",&a,&b);
+        printf("%d
+",a+b); 
+        
+    }
+    
+}
