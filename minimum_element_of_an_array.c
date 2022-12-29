d80a01a7b4ca1e3fcb081b470838ee862daaec96
madhukirankathipalli <119720465+madhukirankathipalli@users.noreply.github.com>
2022-12-29 21:33:50 +0530

    #include<stdio.h> int main() {     int N,i;     scanf("%d",&N);     int a[N];     int max=-9999;     for(i=0;i<N;i++)     {         scanf("%d",&a[N]);         }         if(max<a[i]) {     max=a[i];     }     printf("%d",max);
    }

---
+++ minimum_element_of_an_array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{ 
+    int N,i; 
+    scanf("%d",&N);
+    int a[N];
+    int max=-9999;
+    for(i=0;i<N;i++) 
+    {
+        scanf("%d",&a[N]);
+        }
+        if(max<a[i])
+{
+    max=a[i];
+    }
+    printf("%d",max);
+    
+}
