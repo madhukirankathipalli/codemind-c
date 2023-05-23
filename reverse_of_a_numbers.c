4b4543095a93e6d4448d5a4abd3afa561c3a0eca
KATHIPALLI MADHU KIRAN <119720465+madhukirankathipalli@users.noreply.github.com>
2023-05-23 19:07:16 +0530

    #include<stdio.h> int main() {     int n,rev=0,d;     scanf("%d",&n);     while(n)     {         d=n%10;         rev=rev*10+d;         n=n/10;         }         printf("%d",rev);
    }

--- reverse_of_a_numbers.c
+++ reverse_of_a_numbers.c
@@ -1,13 +1,14 @@
-#include<stdio.h> 
+#include<stdio.h>
 int main()
-{ 
-    int n,r,rev=0;
+{
+    int n,rev=0,d;
     scanf("%d",&n);
-    while(n!=0)
+    while(n)
     {
-        r=n%10;
-        rev=rev*10+r;
+        d=n%10; 
+        rev=rev*10+d;
         n=n/10;
-    }
-    printf("%d",rev);
-}    
+        }
+        printf("%d",rev);
+    
+}
