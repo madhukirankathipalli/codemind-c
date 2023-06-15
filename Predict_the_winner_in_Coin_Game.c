c3ef653812839b9097452c0e71fa73caa25fff79
KATHIPALLI MADHU KIRAN <119720465+madhukirankathipalli@users.noreply.github.com>
2023-06-15 11:54:08 +0530

    #include<stdio.h> int main() {     int m,n;     scanf("%d%d",&m,&n);     if(m%2==0 || n%2==0)     {         printf("Player 1");     }     else     {         printf("Player 2");     } }

--- Predict_the_winner_in_Coin_Game.c
+++ Predict_the_winner_in_Coin_Game.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int n,m;
-    scanf("%d %d",&n,&m);
-    if(n%2==0 || m%2==0)
-    printf("Player 1");
+    int m,n;
+    scanf("%d%d",&m,&n);
+    if(m%2==0 || n%2==0)
+    {
+        printf("Player 1");
+    }
     else
-    printf("Player 2");
+    {
+        printf("Player 2");
+    }
 }
